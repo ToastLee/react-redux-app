1cf6d1655f3525d70195097e03d05c509bea8901
Isaac Lee <dldltkr970@gmail.com>
2024-01-29 18:46:26 +0900

    미들웨이 없이 리덕스 카운터 앱 만들기
    - counter Ui 및 함수 생성
    - Reducer 생성
    - Store 생성 및 Action 전달
    
    CombineReducers
    - root reducer와 sub reducer로 분할
    -store.getState()
    
    Redux Provider
    - Provider로 렌더링
    - Todo UI 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+type Props = {
+  value: any;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({value, onIncrement, onDecrement}: Props) {
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setTodoValue("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      Clicked: {value} times
+      {' '}
+      <button onClick={onIncrement}>
+        +
+      </button>
+      {' '}
+      <button onClick={onDecrement}>
+        -
+      </button>
+      <ul>
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type='text' value={todoValue} onChange={handleChange}/>
+        <input type='submit'/>
+      </form>
+    </p>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
+
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Use Redux'
+})
+console.log('store.getState()', store.getState());
+
+const render = () => root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App
+        value={store.getState().counter}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+      />
+    </Provider>
   </React.StrictMode>
 );
+render();
+
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
